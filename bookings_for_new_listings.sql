b49aba1be2b13bd419de4a239dc4f20a224903f0
BradBaker <brad.baker@airbnb.com>
2013-07-17 12:17:36 +0200

    creating new query

---
+++ bookings_for_new_listings.sql
@@ -0,0 +1,45 @@
+select market,count(*) as bookings
+  ,count(case when created_date> '2013-05-01' then 1 end) as bookings_to_new_listings
+  ,count(case when created_date<= '2013-05-01' then 1 end) as bookings_to_old_listings
+from reservation2s r
+join hostings_summary h
+on h.hosting_id = r.hosting_id 
+where r.status = 1
+and market in ('Santander',
+'Riviera Romagnola',
+'Fuerteventura',
+'Boracay',
+'Mumbai',
+'Belo Horizonte',
+'Kazan',
+'Queenstown',
+'Pattaya',
+'Antalya',
+'La Baule and Noirmoutier',
+'Royan and Lacanau',
+'Rhodes',
+'Yogyakarta',
+'Busan',
+'Gijon',
+'Cebu City',
+'Blue Mountains',
+'Maastricht',
+'Osaka',
+'West Coast North',
+'Savannah',
+'Salzburg',
+'Belfast',
+'Oxford',
+'Sevastopol',
+'Campos do Jordao',
+'Nantucket Region',
+'Hualien',
+'Aarhus',
+'Beijing',
+'Sochi',
+'North Corsica',
+'Utrecht',
+'Manchester',
+'Stuttgart')
+and date(pending_began_at) between '2013-06-01' and '2013-07-01'
+group by 1
